45e099851d206d8a3d567c0bfa1e345614c06bf1
pdegaudenci@hotmail.com <pdegaudenci@hotmail.com>
2023-03-21 12:49:56 -0800

    Agregado control de errores e intents implicitos de camara y URL

--- Practica4_Registro/app/src/main/java/com/example/practica4_registro/MainActivity.java
+++ Practica4_Registro/app/src/main/java/com/example/practica4_registro/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.practica4_registro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.practica4_registro.databinding.ActivityMainBinding;
@@ -34,9 +43,16 @@ public class MainActivity extends AppCompatActivity {
             }
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int before, int count) {
+                boolean valido=false;
+                if (!binding.campoNombre.getText().toString().matches("[A-Za-z]+"))
+                    binding.tilNombre.setError("Debe introducir solamente simbolos alfabeticos");
+                else{
+                    binding.tilNombre.setError(null);
+                    valido=true;
+                }
 
-                if (count>0){
-                    if(binding.campoApellido.length()>0)
+                if (count>0 && valido){
+                    if(!binding.tilApellido.getEditText().getText().toString().isEmpty())
                         binding.btnRegistro.setEnabled(true);
                 }
                 else
@@ -48,29 +64,78 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         binding.campoApellido.addTextChangedListener(new TextWatcher() {
+            boolean valido=false;
             @Override
             public void beforeTextChanged(CharSequence charSequence, int start, int i1, int i2) {
 
             }
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int before, int count) {
+                if (!binding.campoApellido.getText().toString().matches("[A-Za-z]+"))
+                    binding.tilApellido.setError("Debe introducir solamente simbolos alfabeticos");
+                else{
+                    binding.tilNombre.setError(null);
+                    valido=true;
 
-                if (count>0){
-                    if(binding.campoNombre.length()>0)
+                    if (count>0 ){
+
+                    if(!binding.tilApellido.getEditText().getText().toString().isEmpty())
                         binding.btnRegistro.setEnabled(true);
-                }
-            else
-                    binding.btnRegistro.setEnabled(false);}
+
+                } else
+                        binding.btnRegistro.setEnabled(false);
+            }}
             @Override
             public void afterTextChanged(Editable editable) {
 
             }
         });
+        binding.seleccion.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                System.out.println("LONGITUDDD"+(getResources().getStringArray(R.array.lista).length -1));
+                if(i !=  (getResources().getStringArray(R.array.lista).length -1))
+                {
+                    System.out.println("POSICION"+i);
+                    binding.spinner1.setError("Esta app no es para ti");
+                    binding.btnRegistro.setEnabled(false);
+                }
+
+                else
+                    binding.spinner1.setError(null);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        binding.imgRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view instanceof TextView && view.getId()==R.id.img_registro)
+                {
+                    Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(i,0);
+                }
+            }
+        });
+        binding.condicionesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url ="https://developers.google.com/ml-kit/terms";
+                Intent intentWeb = new Intent();
+                intentWeb.setAction(Intent.ACTION_VIEW);
+                intentWeb.setData(Uri.parse("http://"+url));
+                startActivity(intentWeb);
+            }
+        });
     }
 
+
+
     private void binding() {// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.lista, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter =new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.lista));
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
        binding.seleccion.setAdapter(adapter);
         binding.btnRegistro.setOnClickListener(this::validar);
@@ -81,8 +146,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void validar(View view) {
-        if (!binding.campoNombre.toString().matches("[0-9]+")&& !binding.campoApellido.toString().matches("[0-9]+"))
-            Snackbar.make(view,"Los campos no puede tener numeros",Snackbar.LENGTH_LONG).show();
+        if (binding.campoNombre.toString().matches("[A-Za-z]+")&& binding.campoApellido.toString().matches("\"[A-Za-z]+\""))
+            Snackbar.make(view,"Los campos no puede tener numeros o simbolos especiales",Snackbar.LENGTH_LONG).show();
         else
             Snackbar.make(view,"VALIDADO",Snackbar.LENGTH_LONG).show();
 
